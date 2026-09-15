11db239de0c54aa0cd86c8cc3b0887d6493fbcc1
stlynnxx <stevie@eclecticelectronics.com>
2026-09-15 10:03:26 -0500

    Versioning fix

--- slipperypenguin.py
+++ slipperypenguin.py
@@ -4,7 +4,7 @@ from datetime import datetime
 from rich.console import Console
 
 # Version
-__version__ = "2.1.1"
+__version__ = "2.1.2"
 console = Console()
 UPDATE_URL = "https://eclecticelectronics.fly.dev/api/check-update/"
 
